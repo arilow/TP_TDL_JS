3d9e21413220a0648aada250be593774bc331017
Ariel <arilow@live.com.ar>
2022-06-08 20:16:11 -0300

    Gets data from database.

--- codigo/firebase.js
+++ codigo/firebase.js
@@ -4,7 +4,10 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-import { getFirestore, collection, addDoc} from "https://www.gstatic.com/firebasejs/9.8.2/firebase-firestore.js"
+import { getFirestore, 
+        collection, 
+        addDoc,
+        getDocs} from "https://www.gstatic.com/firebasejs/9.8.2/firebase-firestore.js"
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -28,3 +31,5 @@ const store = getFirestore();
 export const add = (name, artist, genere)=> { 
     addDoc(collection(store, 'canciones'), {name, artist, genere});
 };
+
+export const getSongCollection = () => getDocs(collection(store, 'canciones')); 
--- codigo/index.js
+++ codigo/index.js
@@ -1,15 +1,63 @@
-import {add} from './firebase.js'
+import {add, getSongCollection} from './firebase.js'
 
-const form = document.getElementById('form')
-const txtSong = document.getElementById('txtSong')
-const txtArtist = document.getElementById('txtArtist')
-const txtGenere = document.getElementById('txtGenere')
+const songsContainer = document.querySelector(".songs-container");
+let documentFragment = document.createDocumentFragment();
+
+const form = document.getElementById('form');
+const txtSong = document.getElementById('txtSong');
+const txtArtist = document.getElementById('txtArtist');
+const txtGenere = document.getElementById('txtGenere');
 const btnSend = document.getElementById('btnSend');
 
-btnSend.addEventListener('click', (e)=>{
-    e.preventDefault();
-    
-    add(txtSong.value, txtArtist.value, txtGenere.value);
+let arr = []; 
 
-    form.reset();
+const init = () => {
+    arr.forEach(song => {
+        addSong(song.name, song.artist, song.genere);
+    });    
+}
+
+window.addEventListener('DOMContentLoaded', async () => {
+    const querytSnapshot = await getSongCollection();
+    querytSnapshot.forEach(doc => {
+        arr.push(doc.data());
+    });
+    console.log(arr[0]);
+
+    init();
 });
+
+btnSend.addEventListener('click', (e) => {
+    e.preventDefault();
+
+   add(txtSong.value, txtArtist.value, txtGenere.value);
+   form.reset();
+
+    // documentFragment.reset();
+    // songsContainer.reset();
+    // init();
+});
+
+function createSongDescription(nombre, artista, genero) {
+//    img = "<img class='llave-img' src='llave.jpeg'>"
+    nombre = `<h2>${nombre}</h2>`;
+    artista = `<h3>${artista}</h3>`;
+    genero = `<p><b>$${genero}</b></p>`;
+//    return [img, nombre, modelo, precio];
+    return [nombre, artista, genero];
+}
+
+function addSong(nombre, artista, genero) {
+    let songDiv = document.createElement("DIV");
+    let songDescription = createSongDescription(nombre, artista, genero);
+    songDiv.innerHTML = songDescription[0] + songDescription[1] + songDescription[2];
+    
+    documentFragment.appendChild(songDiv);
+    
+    songsContainer.appendChild(documentFragment);    
+}
+
+
+
+
+
